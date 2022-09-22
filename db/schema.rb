f799d347d25e4e83e894b795fe492c3f4a2fe2fc
lomantech <87063237+lomantech@users.noreply.github.com>
2022-09-22 19:25:42 +0300

    add fereign key in the tables

--- db/schema.rb
+++ db/schema.rb
@@ -1,37 +1,46 @@
-ActiveRecord::Schema[7.0].define(version: 20_220_922_115_831) do
-  enable_extension 'plpgsql'
 
-  create_table 'comments', force: :cascade do |t|
-    t.string 'Text'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.integer 'post_id'
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_162338) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table 'likes', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.integer 'post_id'
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'Title'
-    t.text 'Text'
-    t.integer 'CommentsCounter'
-    t.integer 'LikesCounter'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'Name'
-    t.string 'Photo'
-    t.string 'Bio'
-    t.string 'PostsCounter'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.string "bio"
+    t.string "posts_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
