2c761befabdd9913f3e6a0d834bb885d6acd0926
lomantech <87063237+lomantech@users.noreply.github.com>
2022-09-26 11:51:46 +0300

    update post counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
     after_save :update_posts_counter
 
   def update_posts_counter
-    author.increment!(:posts_counter)
+    user_id.increment!(:posts_counter)
   end
 
   def fetch_recent_comments
