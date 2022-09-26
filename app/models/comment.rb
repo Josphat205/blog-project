cc2faf39fa017dda11bebed1408fbc6a587847c4
lomantech <87063237+lomantech@users.noreply.github.com>
2022-09-26 11:37:24 +0300

    Add update comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
     belongs_to :post
     belongs_to :user
+    after_save :update_counter
+  def update_counter
+    post.increment!(:comments_counter)
+  end
 end
