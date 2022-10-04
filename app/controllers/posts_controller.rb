52c4912ae713be7f6a4941a570c84f02212e4c2e
lomantech <87063237+lomantech@users.noreply.github.com>
2022-10-04 12:34:58 +0300

    Add post controller file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,33 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.where(id: [0..3]).order(id: :DESC)
+  before_action :find_user_id, except: [:new]
+  def new; end
+
+  def create
+    @post = @user.posts.create(post_params)
+    if @post.save
+      redirect_to user_posts_path(@user)
+    else
+      render 'new'
+    end
   end
 
-  def new; end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def find_user_id
+    @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
