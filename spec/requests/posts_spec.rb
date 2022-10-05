cfb72e31ad411af50da7ebefd4b2a0e4b05cdf6d
lomantech <87063237+lomantech@users.noreply.github.com>
2022-10-05 08:48:32 +0300

    Add test to check correct placeholder text

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,37 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :request do
+RSpec.describe 'Posts', type: :request do
   before(:each) do
     @user = User.create(name: 'Tom', photo: '', posts_counter: 0)
     @post = Post.create(
       author_id: @user,
-      title: '1st Post!',
-      text: '1st post',
+      title: 'Post title',
+      text: 'Post text',
       comments_counter: 0,
       likes_counter: 0
     )
-    Comment.create(author_id: @user, post_id: @post, text: '1st comment')
-    Comment.create(author_id: @user, post_id: @post, text: '2nd comment')
-    Comment.create(author_id: @user, post_id: @post, text: '3rd comment')
+    Comment.create(author_id: @user, post_id: @post, text: 'This is great')
+    Comment.create(author_id: @user, post_id: @post, text: 'Testing')
+    Comment.create(author_id: @user, post_id: @post, text: 'I love this test')
   end
 
-  describe ':index' do
-    before { get user_posts_path(@user) }
+  describe 'GET /index' do
+    before(:each) do
+      get user_posts_path(@user)
+    end
 
-    it('status should be :ok') do
+    it 'return correct response' do
       expect(response).to have_http_status(:ok)
     end
 
-    it('should render :index template') do
+    it 'should render correct template' do
       expect(response).to render_template(:index)
     end
+
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('Posts:')
+    end
   end
 
-  context ':show' do
-    before { get user_posts_path(@user, @post) }
+  describe 'GET /show' do
+    before(:each) do
+      get user_posts_path(@user, @post)
+    end
 
-    it('status should be :ok') do
+    it 'return correct response' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to_not render_template(:show)
+    end
+
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('Blog')
     end
   end
 end
